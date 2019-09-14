4d2cee37ff66354d58d4404dac952ac9e78ae09a
NoName <skarr.age@mail.ru>
2019-09-14 11:31:30 +0300

    Lesson2 SpringBoot

--- src/main/java/com/example/simple/GreetingController.java
+++ src/main/java/com/example/simple/GreetingController.java
@@ -1,7 +1,11 @@
 package com.example.simple;
 
+import com.example.simple.domain.Message;
+import com.example.simple.repos.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
@@ -9,20 +13,30 @@ import java.util.Map;
 @Controller
 public class GreetingController {
 
+    @Autowired
+    private MessageRepo messageRepo;
+
     @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World")  int age,
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World")  String name,
                            Map<String, Object> model)
     {
-        if(age<18){
-            model.put("name", "Вход воспрещен");
-        }else
-            model.put("name", "Go");
+            model.put("name", name);
         return "greeting";
     }
     @GetMapping
     public String main(Map<String, Object> map){
-        map.put("some", "Main page!");
-        return "mainPage";
+        Iterable<Message> messages = messageRepo.findAll();
+        map.put("messages", messages);
+        return "main";
+    }
+
+    @PostMapping
+    public String addedd(@RequestParam String name, @RequestParam String tag, Map<String, Object> model){
+        Message message = new Message(name, tag);
+        messageRepo.save(message);
+        Iterable<Message> messages = messageRepo.findAll();
+        model.put("messages", messages);
+        return "main";
     }
 
 }
